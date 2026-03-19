2a9927094190af7e283a16af20c743df1610337f
tomgunner1 <tg2026@bath.ac.uk>
2026-03-19 17:23:57 +0000

    Add files via upload

---
+++ final_code.ino
@@ -0,0 +1,54 @@
+int motorL = 12;
+int motorR = 10;
+
+int coilL = A0;
+int coilR = A2;
+
+int baseSpeed = 140;     // forward speed
+float Kp = 0.2;          // steering strength
+int tolerance = 15;      // ignore small noise
+
+void setup() {
+  pinMode(motorL, OUTPUT);
+  pinMode(motorR, OUTPUT);
+
+  Serial.begin(115200);
+}
+
+void loop() {
+
+  int left = analogRead(coilL);
+  int right = analogRead(coilR);
+
+  int error = left - right;
+
+  // ignore tiny differences
+  if (abs(error) < tolerance) {
+    error = 0;
+  }
+
+  int correction = Kp * error;
+
+  int speedL = baseSpeed - correction;
+  int speedR = baseSpeed + correction;
+
+  speedL = constrain(speedL, 0, 255);
+  speedR = constrain(speedR, 0, 255);
+
+  analogWrite(motorL, speedL);
+  analogWrite(motorR, speedR);
+
+  // debugging
+  Serial.print("L:");
+  Serial.print(left);
+  Serial.print(" R:");
+  Serial.print(right);
+  Serial.print(" E:");
+  Serial.print(error);
+  Serial.print(" SL:");
+  Serial.print(speedL);
+  Serial.print(" SR:");
+  Serial.println(speedR);
+
+  delay(10);
+}
